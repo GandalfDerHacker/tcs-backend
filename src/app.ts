4af1b320c0ea7314fc259528302767206f19b564
Armin Windlin <armin.windlin@swisscom.com>
2018-08-24 23:56:02 +0200

    setup changes, added nodemon

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as mongoose from "mongoose";
 import * as route from "./routes/routes";
-import * as oauth from './controller/oauthC';
 
 class App {
 
@@ -17,17 +16,13 @@ class App {
 
     // Setup database connection
     private database(): void {
-        mongoose.connect(process.env.VCAP_SERVICES ?
-            JSON.parse(process.env.VCAP_SERVICES).mongodbent[0].credentials.uri :
-            'mongodb://localhost/ExampleDB');
+        mongoose.connect('mongodb://localhost/TcsTs');
     }
 
     // Middleware
     private config(): void {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({extended: false}));
-        // Remove "//" to activate authentication:
-        //this.app.use(oauth.checkToken);
     }
 
     // Bind routes
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import app from "./app";
 const port = 3000;
 
 // Start server on cloud port or local port 3000
-app.listen(process.env.PORT || port, function () {
+app.listen(port, function () {
     console.log('Express server listening on port ' + port);
 });
