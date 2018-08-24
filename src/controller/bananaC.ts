4270d58a0ae9778aebedfd3719829801d8da61f3
Armin Windlin <armin.windlin@swisscom.com>
2018-08-25 01:42:04 +0200

    added error handling

--- src/controller/bananaC.ts
+++ src/controller/bananaC.ts
@@ -1,21 +1,21 @@
 import {Banana} from "../model/banana";
 
 // Method to add a single banana to database
-export const addBanana = (req, res) => {
+export const addBanana = (req, res, next) => {
     const banana = new Banana(req.body);
     banana.save((err, task) => {
         if (err)
-            res.send(err);
+            return next(err);
         res.send(banana);
         console.info('Added a banana!');
     });
 };
 
 // Method to get all banana from database
-export const getBananas = (req, res) => {
+export const getBananas = (req, res, next) => {
     Banana.find({}, (err, bananas) => {
         if (err) {
-            res.send(err);
+            return next(err);
         }
         res.send(bananas);
         console.info('Sent all bananas!');
@@ -23,10 +23,10 @@ export const getBananas = (req, res) => {
 };
 
 // Method to get a single banana to database
-export const getBanana = (req, res) => {
+export const getBanana = (req, res, next) => {
     Banana.findOne({_id: req.params.id}, (err, banana) => {
         if (err) {
-            res.send(err);
+            return next(err);
         }
         res.send(banana);
         console.info('Sent the requested banana!');
@@ -34,10 +34,10 @@ export const getBanana = (req, res) => {
 };
 
 // Method to delete a single banana to database
-export const deleteBanana = (req, res) => {
+export const deleteBanana = (req, res, next) => {
     Banana.remove({_id: req.params.id}, (err) => {
         if (err) {
-            res.send(err);
+            return next(err);
         }
         res.send({info: "Deleted a banana!"});
         console.info('Deleted a banana!');
